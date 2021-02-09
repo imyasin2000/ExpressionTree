9098396c95304bbc2368d5a94e962fa207970218
Mohammad Hossein Fadavi <mhfa1380@gmail.com>
2021-02-09 11:09:37 +0330

    Improved user interface version.1

--- extree/main/views.py
+++ extree/main/views.py
@@ -6,15 +6,17 @@ def main_views(request):
         return render(request,'main/index.html' ,{'htm':'' ,'expression':' . . . برسی'})
 
     elif request.method=='POST':
+        try:
+            expression=str(request.POST['search'])
+            if expression=='' or expression.isspace():
+                return render(request,'main/index.html' ,{'htm':'' ,'expression':'تعریف نشده'})
+            x=functions.born_children(expression)
+            htm=functions.htmlconvertor(x)
+            htm=htm.replace('<ul></ul>','')
+            posorder_expression=functions.postorder(x)
+            preorder_expression=functions.preorder(x)
+            inorder_expression=functions.inorder(x)
 
-        expression=str(request.POST['search'])
-        if expression=='' or expression.isspace():
-            return render(request,'main/index.html' ,{'htm':'' ,'expression':'یک عبارت صحیح وارد کنید...'})
-        x=functions.born_children(expression)
-        htm=functions.htmlconvertor(x)
-        htm=htm.replace('<ul></ul>','')
-        posorder_expression=functions.postorder(x)
-        preorder_expression=functions.preorder(x)
-        inorder_expression=functions.inorder(x)
-
-        return render(request,'main/index.html',{'htm':htm ,'expression':expression,'postorder':posorder_expression , 'preorder':preorder_expression, 'inorder':inorder_expression})
+            return render(request,'main/index.html',{'htm':htm ,'expression':expression,'postorder':posorder_expression , 'preorder':preorder_expression, 'inorder':inorder_expression})
+        except:
+            return render(request,'main/index.html' ,{'htm':'' ,'expression':'تعریف نشده'})
